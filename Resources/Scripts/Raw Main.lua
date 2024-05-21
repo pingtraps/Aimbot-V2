e3dde648ed4abe16bbf09af401704b753fe7c4fe
pingtraps <vhmung24@gmail.com>
2024-05-20 21:15:33 -0500

    crim settingss

--- Resources/Scripts/Raw Main.lua
+++ Resources/Scripts/Raw Main.lua
@@ -34,7 +34,7 @@ Environment.Settings = {
 	TeamCheck = false,
 	AliveCheck = true,
 	WallCheck = false, -- Laggy
-	Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
+	Sensitivity = 1, -- Animation length (in seconds) before fully locking onto target
 	ThirdPerson = false, -- Uses mousemoverel instead of CFrame to support locking in third person (could be choppy)
 	ThirdPersonSensitivity = 3, -- Boundary: 0.1 - 5
 	TriggerKey = "MouseButton2",
@@ -51,7 +51,7 @@ Environment.FOVSettings = {
 	Transparency = 0.5,
 	Sides = 60,
 	Thickness = 1,
-	Filled = false
+	Filled = true
 }
 
 Environment.FOVCircle = Drawing.new("Circle")
