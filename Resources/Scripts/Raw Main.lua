d162bc1a0ddf9bfe6e2a9095adca556747985fab
pingtraps <vhmung24@gmail.com>
2024-05-20 21:17:24 -0500

    Update Raw Main.lua

--- Resources/Scripts/Raw Main.lua
+++ Resources/Scripts/Raw Main.lua
@@ -223,7 +223,7 @@ function Environment.Functions:ResetSettings()
 		TeamCheck = false,
 		AliveCheck = true,
 		WallCheck = false,
-		Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
+		Sensitivity = 1, -- Animation length (in seconds) before fully locking onto target
 		ThirdPerson = false, -- Uses mousemoverel instead of CFrame to support locking in third person (could be choppy)
 		ThirdPersonSensitivity = 3, -- Boundary: 0.1 - 5
 		TriggerKey = "MouseButton2",
@@ -240,7 +240,7 @@ function Environment.Functions:ResetSettings()
 		Transparency = 0.5,
 		Sides = 60,
 		Thickness = 1,
-		Filled = false
+		Filled = true
 	}
 end
 
